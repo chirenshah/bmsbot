354c0c47de5e5d0b1e5750e1328ce3ec18f0e098
chirenshah <shah.chirn25@gmail.com>
2020-01-18 10:09:13 +0530

    Modularized the program and added a function to check validity of xpath

--- bmsbot.py
+++ bmsbot.py
@@ -4,40 +4,52 @@ from time import sleep
  
 fp = webdriver.FirefoxProfile('/home/chiren/.mozilla/firefox/s7q26fjl.default')
 driver = webdriver.Firefox(fp)
-price_num = []
 theatre = {
 	
 }
 def min():
+	l1 = []
+	c = 1
 	driver.get("https://in.bookmyshow.com/buytickets/tanhaji-the-unsung-warrior-mumbai/movie-mumbai-ET00059932-MT/20200117")
 	#for loop to be added here 
 	venue = driver.find_element_by_xpath("/html/body/section[1]/div[2]/div/ul/li[1]/div[1]/div[2]/div/a")
 	venue_name = venue.text
+	venue_name = { }
+	timing_path = "/html/body/section[1]/div[2]/div/ul/li[1]/div[2]/div[2]/div[{}]".format(c)
+	#while len(driver.find_elements_by_xpath(timing_path)) > 0 :
+	timing_container = len(driver.find_elements_by_class_name("showtime-pill-wrapper"))
+	print(timing_container)
 	timing = driver.find_element_by_xpath("/html/body/section[1]/div[2]/div/ul/li[1]/div[2]/div[2]/div[1]/a/div/div")
 	timing_value = timing.text
 	print(timing_value)
-	driver.find_element_by_xpath("/html/body/section[1]/div[2]/div/ul/li[1]/div[2]/div[2]/div").click() 
-	driver.find_element_by_xpath('//*[@id="btnPopupAccept"]').click()
-	sleep(2)
-	#price_path = ("/html/body/div[19]/strong/div[6]/div[2]/div[3]/div/div[{}]/span[2]".format(i))
-	for i in range (1,3):
-		path = ("/html/body/div[19]/strong/div[6]/div[2]/div[3]/div/div[{}]/span[2]".format(i))
-		price_char = driver.find_element_by_xpath(path)
-		price_char = price_char.text.split()
-		price_num.append(float(price_char[1]))
-		print(price_num)								
-	sleep(1)
-	min_price = min(price_num)
-	theatre[venue_name] = min_price
-	print(theatre)
+	#for i in range (1,10):
+	price_min = check_pricing("/html/body/section[1]/div[2]/div/ul/li[1]/div[2]/div[2]/div[1]")
+	venue_name [timing_value] = price_min
+	print(venue_name)
+	c = c+1
+	timing_path = "/html/body/section[1]/div[2]/div/ul/li[1]/div[2]/div[2]/div[{}]".format(c)
 	driver.close()
 
 def check_exists_by_xpath(xpath):
-    try:
-        driver.find_element_by_xpath(xpath)
-    except Exception as inst:
-        return False
-    return True
+    if (len(driver.find_elements_by_xpath(xpath))) > 0 :
+	return True
+    else:
+	return False
+
+def check_pricing(xpath):
+	price_num = []
+	driver.find_element_by_xpath(xpath).click() 
+	driver.find_element_by_xpath('//*[@id="btnPopupAccept"]').click()
+	sleep(2)
+	for i in range (1,3):
+		for x in range (16,20):
+			path = ("/html/body/div[{}]/strong/div[6]/div[2]/div[3]/div/div[{}]/span[2]".format(x,i))
+			if (len(driver.find_elements_by_xpath(path))) > 0 :
+				price_char = driver.find_element_by_xpath(path)
+				price_char = price_char.text.split() 
+				price_num.append(float(price_char[1]))
+				#res_min = min(price_num)								
+	return (price_num)
 
 if __name__== "__main__":
 	min()
